3df88e43aa388d43e6041d7837545e65ee246983
Denis Fäcke <serayaeryn@gmail.com>
2019-08-28 08:14:59 +0200

    fix typings

--- src/revane-ioc/RevaneIOC.ts
+++ src/revane-ioc/RevaneIOC.ts
@@ -14,6 +14,7 @@ import BeanDefinition from '../revane-ioc-core/BeanDefinition'
 import Loader from '../revane-ioc-core/Loader'
 import { BeanProvider } from '../revane-ioc-core/context/Container'
 
+export * from './loaders/RegexFilter'
 export * from './decorators/Decorators'
 export * from './Options'
 export {
