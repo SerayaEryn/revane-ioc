2da7f959635826c97b80ee7a5a64750a749463bc
Denis Fäcke <serayaeryn@gmail.com>
2020-06-20 18:22:25 +0200

    remove index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const RevaneIOC = require('./bin/src/revane-ioc/RevaneIOC');
-
-module.exports = RevaneIOC.default;
-for (const key of Object.keys(RevaneIOC)) {
-  module.exports[key] = RevaneIOC[key];
-}
