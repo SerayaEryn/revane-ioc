4da24ccaf6c86bc10e9b65f09c5bff04248266cf
Denis Fäcke <serayaeryn@gmail.com>
2025-06-17 15:06:19 +0200

    use String::replaceAll

--- src/revane-configuration/loading/EnvironmentLoader.ts
+++ src/revane-configuration/loading/EnvironmentLoader.ts
@@ -1,11 +1,9 @@
-import { replaceAll } from '../StringReplaceAll.js'
-
 export function replaceEnvironmentVariables (content: string): string {
   let result = content
   for (const name of Object.keys(process.env)) {
     const envValue = process.env[name]
     if (envValue != null) {
-      result = replaceAll(result, `$\{${name}}`, envValue)
+      result = result.replaceAll(`$\{${name}}`, envValue)
     }
   }
   return result
