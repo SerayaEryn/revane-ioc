394ba8b397646f3358380429ff0259431d1f6e7f
Denis Fäcke <serayaeryn@gmail.com>
2025-06-22 10:39:49 +0200

    improve errors

--- src/revane-ioc-core/context/errors/CircularDependencyError.ts
+++ src/revane-ioc-core/context/errors/CircularDependencyError.ts
@@ -2,11 +2,11 @@ export const REV_ERR_CIRCULAR_DEPENDENCY = "REV_ERR_CIRCULAR_DEPENDENCY";
 
 export default class CircularDependencyError extends Error {
   public code = REV_ERR_CIRCULAR_DEPENDENCY;
-  public ids: string[]
+  public ids: string[];
 
   constructor(ids: string[]) {
     super(`bean id=${ids[0]} has circular dependency on itself.`);
     Error.captureStackTrace(this, CircularDependencyError);
-    this.ids = ids
+    this.ids = ids;
   }
 }
--- src/revane-ioc-core/context/errors/DependencyRegisterError.ts
+++ src/revane-ioc-core/context/errors/DependencyRegisterError.ts
@@ -2,12 +2,12 @@ export const REV_ERR_DEPENDENCY_REGISTER = "REV_ERR_DEPENDENCY_REGISTER";
 
 export default class DependencyRegisterError extends Error {
   public code = REV_ERR_DEPENDENCY_REGISTER;
-  public id: string
+  public id: string;
 
   constructor(id: string, error: Error) {
     super("Failed to register dependency id=" + id);
     Error.captureStackTrace(this, DependencyRegisterError);
-    this.id = id
+    this.id = id;
     if (this.stack != null) {
       this.stack += "\nCaused by\n";
       this.stack += error.stack;
