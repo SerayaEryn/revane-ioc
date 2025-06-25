5512f31566c21255209a1cd2d5d23c09d027bc0a
Denis Fäcke <serayaeryn@gmail.com>
2025-06-25 21:43:58 +0200

    improve error

--- src/revane-configuration/TypeMismatch.ts
+++ src/revane-configuration/TypeMismatch.ts
@@ -3,7 +3,7 @@ export const REV_ERR_KEY_TYPE_MISMATCH = "REV_ERR_KEY_TYPE_MISMATCH";
 export class TypeMismatch extends Error {
   public code = REV_ERR_KEY_TYPE_MISMATCH;
 
-  constructor(key: string, type: string) {
+  constructor(public key: string, public type: string) {
     super(`the value for the  key ${key} is not a ${type}`);
     Error.captureStackTrace(this, TypeMismatch);
   }
