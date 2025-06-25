cfdb8c64068986d4d8a57a0255da7b5fbcc0c5ec
Denis Fäcke <serayaeryn@gmail.com>
2025-06-25 17:41:37 +0200

    extend benchmark

--- benchmark/app/App.ts
+++ benchmark/app/App.ts
@@ -1,10 +1,14 @@
 import { Component, Type } from '../../src/revane-ioc/RevaneIOC.js'
 import Dependency1 from './Dependency1.js'
+import Dependency3 from './Dependency3.js'
+import Dependency4 from './Dependency4.js'
 
 @Component
 export default class App {
   constructor (
     @Type(Dependency1) public dependency1: Dependency1,
-    public dependency2) {
+    public dependency2,
+    @Type(Dependency3) public dependency3: Dependency3,
+    @Type(Dependency4) public dependency4: Dependency4,) {
   }
 }
---
+++ benchmark/app/Dependency4.ts
@@ -0,0 +1,7 @@
+import { Component } from '../../src/revane-ioc/RevaneIOC.js'
+
+@Component
+export default class Dependency4 {
+  constructor () {
+  }
+}
