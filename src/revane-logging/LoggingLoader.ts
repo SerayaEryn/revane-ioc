a3a2417086c4e22071d097daee1be536d687ad47
Denis Fäcke <serayaeryn@gmail.com>
2025-07-29 19:54:22 +0200

    refactoring

--- src/revane-logging/LoggingLoader.ts
+++ src/revane-logging/LoggingLoader.ts
@@ -3,9 +3,9 @@ import {
   LoaderOptions,
   BeanDefinition,
   DefaultBeanDefinition,
+  SINGLETON_VALUE,
 } from "../revane-ioc/RevaneIOC.js";
 import { DefaultLogFactory } from "./DefaultLogFactory.js";
-import { SINGLETON_VALUE } from "../revane-ioc-core/Scopes.js";
 
 export class LoggingLoader implements Loader {
   async load(_options: LoaderOptions[]): Promise<BeanDefinition[]> {
